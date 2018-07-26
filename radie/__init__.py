d803dee7fc62fed6e2d2f6526a767ba8bb6a252a
Vince West <dvwest@mmm.com>
2018-07-26 12:50:08 -0500

    bump version

--- radie/__init__.py
+++ radie/__init__.py
@@ -2,7 +2,7 @@ from . import structures, loaders, plugins
 from .structures import StructuredDataFrame
 from .loaders import load_file, load_csv
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 plugins.import_structures()
 plugins.import_loaders()
